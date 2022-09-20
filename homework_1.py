6952dcc96b7cccc36e6ccbbd94b16c841acccb0a
tyfozy11 <108865757+tyfozy11@users.noreply.github.com>
2022-09-20 20:21:15 +0300

    second function

--- homework_1.py
+++ homework_1.py
@@ -7,7 +7,6 @@ def parse(query: str) -> dict:
         return {}
 
 
-
 if __name__ == '__main__':
     assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
     assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
@@ -16,14 +15,15 @@ if __name__ == '__main__':
     assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
 
 
-# def parse_cookie(query: str) -> dict:
-#     parse_cookie_dict = {}
-#
-#     return parse_cookie_dict
-#
-#
-# if __name__ == '__main__':
-#     assert parse_cookie('name=Dima;') == {'name': 'Dima'}
-#     assert parse_cookie('') == {}
-#     assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
-#     assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+def parse_cookie(query: str) -> dict:
+    query = query.rstrip(';').split(';')
+    if len(query) >= 1:
+        return dict(pair.split('=', 1) for pair in query if '=' in pair)
+    return {}
+
+
+if __name__ == '__main__':
+    assert parse_cookie('name=Dima;') == {'name': 'Dima'}
+    assert parse_cookie('') == {}
+    assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
+    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
