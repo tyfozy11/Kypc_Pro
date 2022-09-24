6b14ce98e9f57d6f8cdf161200348da60eef0ed5
tyfozy11 <108865757+tyfozy11@users.noreply.github.com>
2022-09-24 22:36:08 +0300

    complete tasks

--- homwork_2/app.py
+++ homwork_2/app.py
@@ -1,8 +1,11 @@
-from flask import Flask
+import random
+
+from flask import Flask, render_template, request
 from faker import Faker
 import requests
 
 app = Flask(__name__)
+name = Faker('uk_UA')
 
 
 @app.route('/', methods=['get'])
@@ -10,19 +13,30 @@ def index():
     return 'Hello, this is my first project!'
 
 
+@app.route('/requirements', methods=['get'])
+def requirements():
+    requirements = open('requirements.txt', 'r')
+    return requirements
+
+
 @app.route('/generate-users', methods=['get', 'post'])
 def user_generator():
-    name = Faker('uk_UA')
-    for _ in range(100):
-        return name.first_name(), '-', name.email()
+    generation_result = {}
+
+    if request.method == 'POST':
+        users = ""
+        number_of_users = int(request.form['number_of_users'])
+        for fake in range(number_of_users):
+            users += f'\n\n{name.first_name()} - {name.email()}\n'
+            generation_result['users'] = users
+    return render_template('user_generator.html', **generation_result)
 
 
 @app.route('/space', methods=['get'])
 def astro():
     url = ('http://api.open-notify.org/astros.json')
     information_about_astronauts = requests.get(url)
-    space = str(information_about_astronauts.json()['number'])
-    return f'Number of astronauts at the moment is {space} people  '
+    return f"Number of astronauts at the moment is {str(information_about_astronauts.json()['number'])} people"
 
 
 if __name__ == '__main__':
